e54f502ddb55bb0fee4764d8a19831be10f99737
Nicolas LARCHE <nlarche@zeenea.com>
2022-06-23 16:43:59 +0200

    Add maturity to GetVegetableQuery

--- src/domain/query/GetVegetableQuery.spec.ts
+++ src/domain/query/GetVegetableQuery.spec.ts
@@ -3,14 +3,57 @@ import { Vegetable } from "../model/Vegetable";
 import { GetVegetableQuery } from "./GetVegetableQuery";
 
 describe("GetVegetablesQuery", () => {
+  let permaUtils = {
+    getNewDate: () => 0,
+    getNewId: () => "1",
+  };
+
   it("should return a specific vegetables", async () => {
     let vegetables = [
-      { id: { id: "0" }, name: "Tomate" },
-      { id: { id: "1" }, name: "Carotte" },
+      { id: { id: "0" }, name: "Tomate", seedingDate: 1 },
+      { id: { id: "1" }, name: "Carotte", seedingDate: 10 },
     ];
     const repo = new StubVegetableRepository(vegetables as Vegetable[]);
-    const query = new GetVegetableQuery(repo);
+
+    const query = new GetVegetableQuery(repo, permaUtils);
     const vegetable = await query.execute({ id: "0" });
     expect(vegetable.name).toEqual("Tomate");
+    expect(vegetable.maturity).toEqual("JUVENILE");
+  });
+
+  it("should return GROWING vegetable", async () => {
+    let vegetables = [{ id: { id: "0" }, name: "Tomate", seedingDate: 10 }];
+    const repo = new StubVegetableRepository(vegetables as Vegetable[]);
+    const query = new GetVegetableQuery(repo, permaUtils);
+    const vegetable = await query.execute({ id: "0" });
+    expect(vegetable.name).toEqual("Tomate");
+    expect(vegetable.maturity).toEqual("GROWING");
+  });
+
+  it("should return READY vegetable", async () => {
+    let vegetables = [{ id: { id: "0" }, name: "Tomate", seedingDate: 100 }];
+    const repo = new StubVegetableRepository(vegetables as Vegetable[]);
+    const query = new GetVegetableQuery(repo, permaUtils);
+    const vegetable = await query.execute({ id: "0" });
+    expect(vegetable.name).toEqual("Tomate");
+    expect(vegetable.maturity).toEqual("READY");
+  });
+
+  it("should return DYING vegetable", async () => {
+    let vegetables = [{ id: { id: "0" }, name: "Tomate", seedingDate: 150 }];
+    const repo = new StubVegetableRepository(vegetables as Vegetable[]);
+    const query = new GetVegetableQuery(repo, permaUtils);
+    const vegetable = await query.execute({ id: "0" });
+    expect(vegetable.name).toEqual("Tomate");
+    expect(vegetable.maturity).toEqual("DYING");
+  });
+
+  it("should return DEAD vegetable", async () => {
+    let vegetables = [{ id: { id: "0" }, name: "Tomate", seedingDate: 300 }];
+    const repo = new StubVegetableRepository(vegetables as Vegetable[]);
+    const query = new GetVegetableQuery(repo, permaUtils);
+    const vegetable = await query.execute({ id: "0" });
+    expect(vegetable.name).toEqual("Tomate");
+    expect(vegetable.maturity).toEqual("DEAD");
   });
 });
--- src/domain/query/GetVegetableQuery.ts
+++ src/domain/query/GetVegetableQuery.ts
@@ -1,18 +1,44 @@
 import {
+  Maturity,
   Vegetable,
   VegetableId,
   VegetableRepository,
+  VegetableView,
 } from "../model/Vegetable";
-import { Query } from "../../core";
+import { PermaUtils, Query } from "../../core";
 
-export class GetVegetableQuery implements Query<VegetableId, Vegetable> {
+export class GetVegetableQuery implements Query<VegetableId, VegetableView> {
   private vegetableRepository: VegetableRepository;
+  private permaUtils: PermaUtils;
 
-  constructor(vegetableRepository: VegetableRepository) {
+  constructor(
+    vegetableRepository: VegetableRepository,
+    permaUtils: PermaUtils
+  ) {
     this.vegetableRepository = vegetableRepository;
+    this.permaUtils = permaUtils;
   }
 
-  execute(vegetableId: VegetableId): Promise<Vegetable> {
-    return this.vegetableRepository.get(vegetableId);
+  mapToView(vegetable: Vegetable): VegetableView {
+    return {
+      id: vegetable.id,
+      name: vegetable.name,
+      maturity: this.computeMaturity(vegetable.seedingDate),
+    };
+  }
+
+  private computeMaturity(seedingDate: number): Maturity {
+    const today = this.permaUtils.getNewDate();
+    const diff = Math.abs(today - seedingDate);
+    if (diff <= 1) return "JUVENILE";
+    if (diff <= 10) return "GROWING";
+    if (diff <= 100) return "READY";
+    if (diff <= 150) return "DYING";
+    return "DEAD";
+  }
+
+  async execute(vegetableId: VegetableId): Promise<VegetableView> {
+    const vegetable = await this.vegetableRepository.get(vegetableId);
+    return this.mapToView(vegetable);
   }
 }
